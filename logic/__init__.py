16f973698fe33abfbd5dcf769f3ad64246ecb457
TheBSquare <minka.denis@gmail.com>
2021-04-29 14:32:06 +0300

    update

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,3 +1,3 @@
-from youtube_scripts.youtube_parser import *
-from youtube_scripts.additional import *
+from .youtube_scripts.youtube_parser import *
+from .youtube_scripts.additional import *
 from .files_handler import *
